dfd054c77f242d81b96e4c4d884bdaa2ee08bc63
Gordon Huang <dqna64@gmail.com>
2022-07-25 02:41:30 +1000

    create autotest script

---
+++ autotest.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+parameters="
+	default_compilers = {'c' : [['clang', '-Werror', '-std=gnu11', '-g', '-lm']]}
+	upload_url = https://example.com/autotest.cgi
+"
+target="$(readlink -f "$0")"
+targetDir="$(dirname "$target")"
+
+exec "/usr/local/bin/autotest/autotest.py" --exercise_directory "${targetDir}/problems" --parameters "$parameters" "$@"
+
+
