f81a3b482b5021b2331a75b0b0de80ee10a940fc
z5416829 <z5416829@cse.unsw.edu.au>
2022-07-29 23:05:27 +1000

    sort_the_trio

---
+++ problems/sort_the_trio/autotest/Sort.h
@@ -0,0 +1,13 @@
+// Header file for sort_the_trio
+
+#ifndef SORT_H
+#define SORT_H
+
+//Helper function to swap elements 'array[i]' and 'array[j]' in array. 
+void swap (int *array, int i, int j);
+
+//Sorts the array of ints
+void sort(int *array, int high);
+
+#endif
+
