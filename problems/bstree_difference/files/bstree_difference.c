94fac74a986abc0405acb80b2b4e51e546f6e8b4
Isaiah Iliffe <isaiahiliffe@gmail.com>
2022-07-31 01:08:53 +1000

    bstree_difference: add main.md, shorten stub code comment

--- problems/bstree_difference/files/bstree_difference.c
+++ problems/bstree_difference/files/bstree_difference.c
@@ -12,27 +12,6 @@
  * Determines the minimum difference between any two values in the given
  * binary search tree. You may assume that the BST has at least two nodes.
  * The testing code reads the preorder traversal of the BST from stdin.
- *
- * For example, if the preorder traversal is 11 7 4 20 18 23 30, the BST is
- *      11
- *     /  \
- *    /    \
- *   7     20
- *  /     /  \
- * 4     /    \
- *      18    23
- *              \
- *              30
- * and the answer is 20 - 18 = 2.
- *
- * Any efficient solution, such as one that runs in O(n) or O(n log n), should
- * pass the autotests, but the following are some extra challenges:
- *   - Solve the problem using O(h) additional space, where h is the height
- *     of the BST. Hint: consider performing an inorder traversal of the BST.
- *   - Solve the problem using O(1) additional space. Hint: think about what
- *     functionality the provided bstree_node struct offers.
- * Additional space refers to any space allocated between the time that the
- * function bstree_difference is called and the time it returns.
  * 
  * Input:
  *   - bstree: a pointer to the root of the BST, see bstree.h for more info.
@@ -41,5 +20,6 @@
  */
 
 int bstree_difference(bstree t) {
-	return 7;
+	// TODO: Implement this function
+	return 2;
 }
