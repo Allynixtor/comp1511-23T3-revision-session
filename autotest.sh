3fdaa379da5e0029a332415e0304412cdf153eed
Gordon Huang <dqna64@gmail.com>
2022-11-09 00:31:25 +1100

    fix autotest

--- autotest.sh
+++ autotest.sh
@@ -7,6 +7,6 @@ parameters="
 target="$(readlink -f "$0")"
 targetDir="$(dirname "$target")"
 
-exec "/usr/local/bin/autotest/autotest.py" --exercise_directory "${targetDir}/problems" --parameters "$parameters" "$@"
+python3 "/usr/local/bin/autotest/autotest.py" --exercise_directory "${targetDir}/problems" --parameters "$parameters" "$@"
 
 
