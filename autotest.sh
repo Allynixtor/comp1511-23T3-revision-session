fe7fd4d2b921847b047d8ce4d2b2dc264b0c7f69
Allynixtor <bunchymovie123@gmail.com>
2022-11-11 12:58:45 +1100

    fix autotest marc_coin

--- autotest.sh
+++ autotest.sh
@@ -4,7 +4,7 @@ parameters="
 	default_compilers = {'c' : [['clang', '-Werror', '-std=gnu11', '-g', '-lm']]}
 	upload_url = https://example.com/autotest.cgi
 "
-target="$(readlink -f "$0")"
+target="$(readlink -n "$0")"
 targetDir="$(dirname "$target")"
 
 python3 "/usr/local/bin/autotest/autotest.py" --exercise_directory "${targetDir}/problems" --parameters "$parameters" "$@"
